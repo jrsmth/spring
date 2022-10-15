2286568ee083bb61aa13c787d562b758f2bf89f3
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-10-15 20:16:06 +0100

    [05-82] Pet Clinic - List owners, closes #14

--- content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/controller/OwnerController.java
+++ content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/controller/OwnerController.java
@@ -1,14 +1,20 @@
 package com.jrsmiffy.springguru.petclinic.controller;
 
+import com.jrsmiffy.springguru.petclinic.service.OwnerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller @RequiredArgsConstructor
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
     @RequestMapping({"/", ""})
-    public String getOwners(){
+    public String getOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 
