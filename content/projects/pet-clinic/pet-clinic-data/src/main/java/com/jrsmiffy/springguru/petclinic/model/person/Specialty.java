de01e48e1a1bbd3b6d5d3ae19aae3b796b5c6d50
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-11-05 17:36:04 +0000

    [07-119] Pet Clinic - Creating specialty and associate with vet, closes #21

--- content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/person/Specialty.java
+++ content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/person/Specialty.java
@@ -1,17 +1,12 @@
 package com.jrsmiffy.springguru.petclinic.model.person;
 
 import com.jrsmiffy.springguru.petclinic.model.BaseEntity;
-import com.jrsmiffy.springguru.petclinic.model.pet.Pet;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Getter @Setter // @Data?
-public class Visit extends BaseEntity {
+public class Specialty extends BaseEntity {
 
-    private LocalDate date; // question: would a unix timestamp be better?
     private String description;
-    private Pet pet;
 
 }
--- content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/person/Vet.java
+++ content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/person/Vet.java
@@ -1,3 +1,13 @@
 package com.jrsmiffy.springguru.petclinic.model.person;
 
-public class Vet extends Person { }
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter @Setter // @Data?
+public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+}
