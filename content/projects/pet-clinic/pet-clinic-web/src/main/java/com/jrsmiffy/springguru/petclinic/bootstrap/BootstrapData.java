ca7c31294041d645e8e47aeb9e3ac266bb75af4f
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-11-09 21:31:07 +0000

    [07-122] Bootstrap PetType data, closes #26

--- content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/bootstrap/BootstrapData.java
+++ content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/bootstrap/BootstrapData.java
@@ -2,7 +2,9 @@ package com.jrsmiffy.springguru.petclinic.bootstrap;
 
 import com.jrsmiffy.springguru.petclinic.model.person.Owner;
 import com.jrsmiffy.springguru.petclinic.model.person.Vet;
+import com.jrsmiffy.springguru.petclinic.model.pet.PetType;
 import com.jrsmiffy.springguru.petclinic.service.OwnerService;
+import com.jrsmiffy.springguru.petclinic.service.PetTypeService;
 import com.jrsmiffy.springguru.petclinic.service.VetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +15,31 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
 
     private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
     private final VetService vetService;
 
     @Override
     public void run(String... args) {
+        loadPetType();
+        loadOwner();
+        loadVet();
+
+    }
+
+    private void loadPetType() {
+        PetType dogType = new PetType();
+        dogType.setType("Dog");
+        PetType savedDogType = petTypeService.save(dogType);
+
+        PetType catType = new PetType();
+        catType.setType("Cat");
+        PetType savedCatType = petTypeService.save(catType);
+
+        log.info("--- Data Loaded :: Pet Type ---");
+
+    }
+
+    private void loadOwner(){
         Owner ownerOne = new Owner();
         ownerOne.setId(1L);
         ownerOne.setFirstName("Michael");
@@ -31,6 +54,9 @@ public class BootstrapData implements CommandLineRunner {
 
         log.info("--- Data Loaded :: Owner ---");
 
+    }
+
+    private void loadVet() {
         Vet vetOne = new Vet();
         vetOne.setId(1L);
         vetOne.setFirstName("Sam");
