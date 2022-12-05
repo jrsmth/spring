c204143b3f27cfc812a4358000f9155d3a7ef83c
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-05 08:42:02 +0000

    [08-140] Query Methods

--- content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/repository/CategoryRepository.java
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/repository/CategoryRepository.java
@@ -3,6 +3,15 @@ package com.jrsmiffy.springguru.recipes.repository;
 import com.jrsmiffy.springguru.recipes.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+    // Note: This is called a 'Query Method'
+    // Note: Spring Data JPA will handle the Hibernate plumbing behind the scenes and implement this for us
+    // Question: What is the equivalent implementation if you do it yourself?
+
+    // Note: Use of Optional<T> was added in Spring 5
 
 }
--- content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/repository/UnitRepository.java
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/repository/UnitRepository.java
@@ -3,6 +3,10 @@ package com.jrsmiffy.springguru.recipes.repository;
 import com.jrsmiffy.springguru.recipes.model.Unit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitRepository extends CrudRepository<Unit, Long> {
+
+    Optional<Unit> findByName(String name);
 
 }
