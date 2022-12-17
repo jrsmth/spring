5e32048afced1acf4f50bc586f16dddb7c4d264c
JRSmiffy <jamesrichardsmith97@gmail.com>
2022-12-17 16:52:31 +0000

    [08-156] Pet Clinic - Added Visit Map service, closes #38

---
+++ content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package com.jrsmiffy.springguru.petclinic.service;
+
+import com.jrsmiffy.springguru.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+
+}
---
+++ content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.jrsmiffy.springguru.petclinic.service.map;
+
+import com.jrsmiffy.springguru.petclinic.model.Visit;
+import com.jrsmiffy.springguru.petclinic.service.VisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service @RequiredArgsConstructor @Profile("map")
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
