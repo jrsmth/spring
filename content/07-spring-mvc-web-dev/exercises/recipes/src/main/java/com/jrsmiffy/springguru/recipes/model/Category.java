b2473e796eae545db40b73cc1202e5f3af6e6423
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-01-06 19:24:08 +0000

    [09-166] Lombok gotchas

--- content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Category.java
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Category.java
@@ -3,7 +3,11 @@ package com.jrsmiffy.springguru.recipes.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.Set;
 
 @Entity @Getter @Setter
--- content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Ingredient.java
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Ingredient.java
@@ -1,12 +1,13 @@
 package com.jrsmiffy.springguru.recipes.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @NoArgsConstructor
 public class Ingredient {
 
     public Ingredient(String description, BigDecimal quantity, Unit unit) {
