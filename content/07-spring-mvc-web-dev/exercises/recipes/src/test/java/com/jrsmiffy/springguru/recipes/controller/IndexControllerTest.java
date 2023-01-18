1c59a88b94db4889570049992b953eb26e1d14e2
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-01-18 19:09:39 +0000

    [10-174] Assigment - IndexController

---
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/test/java/com/jrsmiffy/springguru/recipes/controller/IndexControllerTest.java
@@ -0,0 +1,48 @@
+package com.jrsmiffy.springguru.recipes.controller;
+
+import com.jrsmiffy.springguru.recipes.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/** Unit Test for {@link IndexController} */
+public class IndexControllerTest {
+
+    private IndexController underTest;
+
+    @Mock private RecipeService mockService;
+    @Mock private Model model;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        underTest = new IndexController(mockService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        // given
+        final String template = "index";
+
+        // when
+        when(mockService.getRecipes()).thenReturn(Set.of());
+
+        String result = underTest.getIndexPage(model);
+
+        // then
+        assertEquals(template,result);
+
+        verify(mockService, times(1)).getRecipes();
+    }
+
+}
