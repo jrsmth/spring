caad24d4d7fb6efe58dd162e0012ed1e205ea91d
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-01-30 17:50:39 +0000

    [10-178] Spring IT

--- content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Unit.java
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/java/com/jrsmiffy/springguru/recipes/model/Unit.java
@@ -1,11 +1,13 @@
 package com.jrsmiffy.springguru.recipes.model;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity @Getter
 public class Unit {
 
     @Id
---
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/test/java/com/jrsmiffy/springguru/recipes/repository/UnitRepositoryIT.java
@@ -0,0 +1,36 @@
+package com.jrsmiffy.springguru.recipes.repository;
+
+import com.jrsmiffy.springguru.recipes.model.Unit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class) @DataJpaTest
+public class UnitRepositoryIT {
+
+    @Autowired
+    UnitRepository unitRepository;
+
+    @Test
+    public void findByName() {
+        // Given
+        Unit unit = null;
+
+        // When
+        Optional<Unit> result = unitRepository.findByName("Teaspoon");
+
+        // Then
+        if (result.isPresent()) unit = result.get();
+
+        assert unit != null;
+        assertEquals("Teaspoon", unit.getName());
+    }
+
+
+}
