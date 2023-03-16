91fd108a22bbcc230766e3922e61435e25dbee7e
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-03-16 07:52:31 +0000

    [11-213] Pet Clinic :: Fix display owner

--- content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/PetType.java
+++ content/projects/pet-clinic/pet-clinic-data/src/main/java/com/jrsmiffy/springguru/petclinic/model/PetType.java
@@ -2,11 +2,17 @@ package com.jrsmiffy.springguru.petclinic.model;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import javax.persistence.Entity;
 
 @Entity @Getter @Setter // Note: @Builder doesn't seem worth it here
 public class PetType extends BaseEntity {
 
     private String type;
+
+    @Override
+    public String toString() {
+        return type;
+    }
 
 }
