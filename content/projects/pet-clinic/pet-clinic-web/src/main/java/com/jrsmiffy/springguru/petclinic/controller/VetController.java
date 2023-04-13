2c19fc89434ae6cf934b4c2ca16f195bffc0fe4e
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-04-13 08:20:02 +0100

    [12-245] Pet Clinic :: Vet JSON endpoint, closes #53

--- content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/controller/VetController.java
+++ content/projects/pet-clinic/pet-clinic-web/src/main/java/com/jrsmiffy/springguru/petclinic/controller/VetController.java
@@ -1,21 +1,33 @@
 package com.jrsmiffy.springguru.petclinic.controller;
 
+import com.jrsmiffy.springguru.petclinic.model.Vet;
 import com.jrsmiffy.springguru.petclinic.service.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller @RequiredArgsConstructor
-@RequestMapping("/vet")
+//@RequestMapping("/vet")
 public class VetController {
 
     private final VetService vetService;
 
-    @RequestMapping({"/", ""})
+    //@RequestMapping({"/", ""})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    // Note :: I suppose this should be plural "vet(s)", as we are dealing with all vets, rather than an individual
     public String getVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vet/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
