56379b8380bed6f625dd3e3683179c5bec89ec2f
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-05-08 18:51:12 +0100

    [18-307] Set-up MySQL database for recipes app

---
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/resources/schema-dev.sql
@@ -0,0 +1,23 @@
+-- ---------------- --
+-- Create Databases --
+-- ---------------- --
+# CREATE DATABASE spring_recipe_dev; -- create first, connect via IntelliJ and then run the script below
+
+-- ----------------------- --
+-- Create service accounts --
+-- ----------------------- --
+CREATE USER 'spring_recipe_dev_user'@'localhost' IDENTIFIED BY 'secret-dev';
+CREATE USER 'spring_recipe_dev_user'@'%' IDENTIFIED BY 'secret-dev';
+# Question :: Why do we use both @'localhost' and @'%', rather than just the latter?
+
+-- --------------- --
+-- Database Grants --
+-- --------------- --
+GRANT SELECT ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'localhost';
+GRANT INSERT ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'localhost';
+GRANT DELETE ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'localhost';
+GRANT UPDATE ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'localhost';
+GRANT SELECT ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'%';
+GRANT INSERT ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'%';
+GRANT DELETE ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'%';
+GRANT UPDATE ON spring_recipe_dev.* to 'spring_recipe_dev_user'@'%';
---
+++ content/07-spring-mvc-web-dev/exercises/recipes/src/main/resources/schema-prod.sql
@@ -0,0 +1,23 @@
+-- ---------------- --
+-- Create Databases --
+-- ---------------- --
+# CREATE DATABASE spring_recipe_prod; -- create first, connect via IntelliJ and then run the script below
+
+-- ----------------------- --
+-- Create service accounts --
+-- ----------------------- --
+CREATE USER 'spring_recipe_prod_user'@'localhost' IDENTIFIED BY 'secret-prod';
+CREATE USER 'spring_recipe_prod_user'@'%' IDENTIFIED BY 'secret-prod';
+# Question :: Why do we use both @'localhost' and @'%', rather than just the latter?
+
+-- --------------- --
+-- Database Grants --
+-- --------------- --
+GRANT SELECT ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'localhost';
+GRANT INSERT ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'localhost';
+GRANT DELETE ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'localhost';
+GRANT UPDATE ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'localhost';
+GRANT SELECT ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'%';
+GRANT INSERT ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'%';
+GRANT DELETE ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'%';
+GRANT UPDATE ON spring_recipe_prod.* to 'spring_recipe_prod_user'@'%';
