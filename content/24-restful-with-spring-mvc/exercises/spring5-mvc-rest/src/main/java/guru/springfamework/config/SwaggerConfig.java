93179f3a163d0bc7e4fd056fac3078dc08199151
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-08-11 07:09:24 +0100

    [25-399] Configure swagger meta-data

--- content/24-restful-with-spring-mvc/exercises/spring5-mvc-rest/src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ content/24-restful-with-spring-mvc/exercises/spring5-mvc-rest/src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,7 +23,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     } // Note :: The Docket API gives us control over what info we expose through Swagger
 
     // Note :: Go to http://localhost:8080/v2/api-docs for JSON info about the APIs
@@ -36,5 +41,23 @@ public class SwaggerConfig {
      // Note :: Spring Boot will configure the swagger ui for us
      // Note :: w/o Spring Boot, you would need to add the resource handlers above
     */
+
+    // Note :: here we are customising the meta-data of our Swagger API docs
+    private ApiInfo metaData(){
+        return new ApiInfo(
+                "Spring Framework Guru",
+                "Spring Framework 5: Beginner to Guru",
+                "1.0",
+                "Terms of Service: blah",
+                new Contact(
+                    "John Thompson",
+                    "https://springframework.guru/about/",
+                    "john@springfrmework.guru"
+                ),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
+    }
 
 }
